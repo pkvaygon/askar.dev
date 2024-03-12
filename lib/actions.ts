fdd4ceb51935d9a0c34ad51a8b0a37352e28d78f
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-03-12 01:55:35 -0500

    resend hide code

--- lib/actions.ts
+++ lib/actions.ts
@@ -17,15 +17,15 @@ export async function onContactMeFormSubmit(formData: FormData) {
   };
 
   try {
-    const response = await fetch(
-      "http://localhost:3000/api/send",
-      requestOptions
-    );
-    if (!response.ok) {
-      throw new Error("Ошибка при выполнении запроса");
-    }
+    // const response = await fetch(
+    //   "http://localhost:3000/api/send",
+    //   requestOptions
+    // );
+    // if (!response.ok) {
+    //   throw new Error("Ошибка при выполнении запроса");
+    // }
     cookies().set("sent", "true", { expires: expirationDate });
-    return response.json();
+    // return response.json();
   } catch (error) {
     console.error("Произошла ошибка:", error);
   }
